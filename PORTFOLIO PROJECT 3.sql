9bd45ac3403e180be8766b91f830848ee1ce9bee
Egunjobi Tunde <egunjobitunde20@gmail.com>
2022-06-16 20:53:31 +0100

    Update PORTFOLIO PROJECT 3.sql

--- PORTFOLIO PROJECT 3.sql
+++ PORTFOLIO PROJECT 3.sql
@@ -1,7 +1,7 @@
 --DATA CLEANING WITH SEQUEL
 /* Data cleaning also known as data cleansing is the process of detecting and correcting inacccurate or duplicate records
-in a database. It is part of the most important parts of a data analysis, before analysis can be made it is essential that
-the data undergo data cleaning else it may lead to wrong analysis and result which leads to bad decision*/
+in a database. It is one of the most important parts of a data analysis before analysis can be made it is essential that
+the data undergo data cleaning else it may lead to wrong analysis and result which lead to bad decision*/
 
 
 SELECT *
